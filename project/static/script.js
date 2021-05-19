7b96f407870ad9d3d18abb504c958bb3dd8fecb8
Facu <bestjayceeu@gmail.com>
2021-05-19 16:25:18 +0200

    Now it sends 2 differents draws

--- project/static/script.js
+++ project/static/script.js
@@ -1,4 +1,8 @@
 let penColour = 'black';
+let selectedDraw = 1;
+let drawOne = [];
+let drawTwo = [];
+let actualDraw = [];
 
 function setPixelColour(pixel){
 
@@ -15,24 +19,90 @@ function setPenColour(color) {
   document.getElementById("actualColor").style.backgroundColor = penColour;
 }
 
-function sendDraw(){
-
+function getDraw(){
   const x = document.getElementsByClassName("pixel");
-  let colors = [];
+
+  let draw = [];
+
 
   for (let i = 0; i < x.length; i++) {
 
     x[i].getAttribute("value") == null
-      ? colors.push("blank")
-      : colors.push(x[i].getAttribute("value"))
+      ? draw.push("blank")
+      : draw.push(x[i].getAttribute("value"))
   }
 
-  console.log(colors);
+  return draw;
+}
+
+function sendDraw(){
+
+  draw = getDraw();
+
+  console.log(draw);
 
   const URL = '/profile'
   const xhr = new XMLHttpRequest();
-  sender = JSON.stringify(colors)
+  sender = JSON.stringify(draw)
   xhr.open('POST', URL);
   xhr.send(sender);
 
+}
+
+function addDraw() {
+  if($('#buttonList').find('button').length < 3) 
+    $('#buttonList').append('<button class="button" onclick="selectDraw(this)">2</button>');
+  else 
+    alert('Ya no caben más items en el contenedor!');
+  return false;  
+}
+
+function selectDraw(button) {
+
+  deleteDraw();
+
+  selectedDraw = button.innerHTML;
+
+  selectedDraw == 1 
+  ?  actualDraw = drawOne
+  :  actualDraw = drawTwo; 
+
+  var x = document.getElementsByClassName("pixel");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.backgroundColor = actualDraw[i];
+  }
+
+}
+
+function getActualDraw(){
+  let actualDraw = [];
+
+  $( "#art .pixel" ).each(function( index, elem ) {
+
+    $(elem).css("background-color") == null
+    ? actualDraw.push("blank")
+    : actualDraw.push($(elem).css("background-color"))
+
+
+  });
+
+  console.log(actualDraw);
+
+  return actualDraw;
+}
+
+
+function saveDraw(){
+
+  savedDraw = getActualDraw();
+
+  selectedDraw == 1 ? drawOne = savedDraw : drawTwo = savedDraw;
+}
+
+function deleteDraw(){
+  var x = document.getElementsByClassName("pixel");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.backgroundColor = "";
+    x[i].setAttribute("value","blank");
+  }
 }
