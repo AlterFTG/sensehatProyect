afaa2e9ef745231a2a7e8b64d24ec663a04dbd82
Facu <bestjayceeu@gmail.com>
2021-06-03 14:07:51 +0000

    Translate texts

--- project/auth.py
+++ project/auth.py
@@ -27,16 +27,17 @@ def signup_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    user = User.query.filter_by(email=email).first() # Si devuelve un usuario es que ya existe en la base de datos.
+                                                        
 
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
+    if user: # Si este usuario existe, hacemos una redirección a la página de sign up para que lo intente otra vez.
         flash('Email address already exists')
         return redirect(url_for('auth.signup'))
 
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    # Crea un nuevo usuario en base a la información del formulario. Se hashea el password para que no se guarde en un texto plano.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
 
-    # add the new user to the database
+    # Agrega un nuevo usuario a la base de datos.
     db.session.add(new_user)
     db.session.commit()    
 
@@ -50,13 +51,13 @@ def login_post():
 
     user = User.query.filter_by(email=email).first()
 
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    # Comprueba si el usuario existe
+    # Recibe el password introducido por el usuario, lo hashea y lo compara con el otro password hasheado en la base de datos.
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login')) # Si el usuario no existe o si el password es incorrecto, se recarga esta página.
 
-    # if the above check passes, then we know the user has the right credentials
+    # Si el check de arriba sabemos que el usuario escribió la contraseña correcta.
     login_user(user, remember=remember)
     return redirect(url_for('main.draw'))
 
